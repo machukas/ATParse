d04f0b79cc892e6eee9e6b00f7121a34b396c43e
Nico Landa <nico.aratech@gmail.com>
2016-12-21 10:42:31 +0100

    Initial Commit

---
+++ ATParse/ATParse.h
@@ -0,0 +1,19 @@
+//
+//  ATParse.h
+//  ATParse
+//
+//  Created by Aratech iOS on 21/12/16.
+//  Copyright © 2016 AraTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATParse.
+FOUNDATION_EXPORT double ATParseVersionNumber;
+
+//! Project version string for ATParse.
+FOUNDATION_EXPORT const unsigned char ATParseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATParse/PublicHeader.h>
+
+
